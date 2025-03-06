4df2a83ac2424d678cef9826f1111e79c1654e38
Yuvaraj7117 <danielguna494@gmail.com>
2025-03-06 21:59:43 +0530

    Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,19 @@
+import AdminDashBoard from "./pages/AdminDashBoard";
 import DashBoard from "./pages/DashBoard";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function App() {
-
-  
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashBoard/>} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/admin" element={<AdminDashBoard />} />
         </Routes>
       </BrowserRouter>
     </>
--- Frontend/src/pages/DashBoard.jsx
+++ Frontend/src/pages/DashBoard.jsx
@@ -35,10 +35,19 @@ export default function DashBoard() {
       <Navbar logoutUser={logoutUser} />
 
       <div className="top">
-        <span className="h2">Hello,{user.name}</span>
-        <input type="search" className="form-control" placeholder="Search everything......." />
+        <span className="h2 text-danger">
+          Hello,{user.name}
+          <br />
+          <hr />
+          <span className="text-primary">{user.name}</span>
+        </span>
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Search everything......."
+        />
       </div>
-      <main role="main" className="container mt-5">
+      {/* <main role="main" className="container mt-5">
         <div className="container">
           <div className="text-center mt-5">
             <h3>Dashboard page</h3>
@@ -54,6 +63,99 @@ export default function DashBoard() {
             )}
           </div>
         </div>
+      </main> */}
+      <main>
+        <form action="#">
+          <table className="table table-bordered text-center">
+            <tr>
+              <th>Tablet Name</th>
+              <th>Expiry Date</th>
+              <th>Available Quantity</th>
+              <th>Price(per unit)</th>
+              <th>Needed Quantity</th>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="group">
+                <input type="number" className="form-control" />
+                <button className="btn btn-primary">Purchase</button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="group">
+                <input type="number" className="form-control" />
+                <button className="btn btn-primary">Purchase</button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="group">
+                <input type="number" className="form-control" />
+                <button className="btn btn-primary">Purchase</button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="group">
+                <input type="number" className="form-control" />
+                <button className="btn btn-primary">Purchase</button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="group">
+                <input type="number" className="form-control" />
+                <button className="btn btn-primary">Purchase</button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="group">
+                <input type="number" className="form-control" />
+                <button className="btn btn-primary">Purchase</button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="group">
+                <input type="number" className="form-control" />
+                <button className="btn btn-primary">Purchase</button>
+              </td>
+            </tr>
+          </table>
+          <div className="billSection">
+            <span>Total Amount:</span>
+            <input type="number" className="form-control w-25 d-inline" />
+            <button type="reset" className="btn btn-danger">
+              Cancel
+            </button>
+            <button type="submit" className="btn btn-success">
+              Generate Bill
+            </button>
+          </div>
+        </form>
       </main>
     </>
   );
