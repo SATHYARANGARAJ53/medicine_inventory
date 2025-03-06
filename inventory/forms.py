36e93625a01396be7b314c2d06e15e3cbc889358
Sathya R <143745156+SATHYARANGARAJ53@users.noreply.github.com>
2025-03-06 23:38:49 +0530

    Delete inventory directory

--- inventory/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Medicine
-
-class MedicineForm(forms.ModelForm):
-    class Meta:
-        model = Medicine
-        fields = ['name', 'price', 'expiry_date', 'quantity_available']
